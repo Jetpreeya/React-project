34b521f9284b623e73f900ba7007183dbfa051e9
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-02-19 22:53:06 +0100

    Finished with register part

--- web-ecommerce/src/component/buttons/register.jsx
+++ web-ecommerce/src/component/buttons/register.jsx
@@ -2,48 +2,55 @@ import React from 'react';
 
 const Register = () => {
     return (
-        <div>
-            <button type="button" className="btn btn-outline-dark ms-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <i className="fa fa-user me-1 fa-2x"></i> Register
+        <>
+            <button type="button" className="btn btn-outline-info ms-2" data-bs-toggle="modal" data-bs-target="#NewModal">
+            <i className="fa fa-sign-in me-1 fa-2x"></i> Register
             </button>
 
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal fade" id="NewModal" tabindex="-1" aria-labelledby="NewModal" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel"> Registration </h5>
+                            <h5 class="modal-title" id="NewModal">Registration</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                             <form>
-                                <div class="mb-3">
-                                    <label for="exampleFormControlInput1" class="form-label">First Name</label>
-                                    <input type="text" class="form-control" id="exampleFormControlInput1" />
+                            <div class="mb-3">
+                                    <label for="NewInputName" class="form-label">First Name</label>
+                                    <input type="text" class="form-control" id="NewInputName"/>
                                 </div>
                                 <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Last Name </label>
-                                    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <label for="NewInputLast" class="form-label">Last Name</label>
+                                    <input type="text" class="form-control" id="NewInputLast"/>
                                 </div>
                                 <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label"> Email </label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                    <label for="NewInputEmail1" class="form-label">Email address</label>
+                                    <input type="email" class="form-control" id="NewInputEmail1" aria-describedby="emailHelp"/>
                                 </div>
                                 <div class="mb-3">
-                                    <label for="exampleInputPassword1" class="form-label">Password</label>
-                                    <input type="password" class="form-control" id="exampleInputPassword1" />
+                                    <label for="NewInputPassword1" class="form-label">Password</label>
+                                    <input type="password" class="form-control" id="NewInputPassword1"/>
                                 </div>
-                                <div class="mb-3 form-check">
-                                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                                    <label class="form-check-label" for="exampleCheck1">Agree to the terms</label>
-                                </div>
-                                <button type="submit" class="btn btn-primary">Register</button>
-                                <button type="submit" class="btn btn-primary">Cancle</button>
+        
+                                <button type="submit" className="btn btn-success w-30 ms-3 ">Register</button>
+                                <button type="submit" className="btn btn-danger w-30 ms-3">Cancle</button>
+
+                                <div className= "modal-body">
+                            <button className="btn btn-primary w-100 mb-4">
+                                <span className="fa fa-google me-2"></span> Register With Google 
+                            </button>
+                            <button className="btn btn-primary w-100 mb-2">
+                                <span className="fa fa-facebook me-2"></span> Register With Facebook 
+                            </button>
+                            </div>
                             </form>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
+
     )
 }
 
