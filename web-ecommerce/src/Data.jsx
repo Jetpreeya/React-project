9946fe46577341980064e8c0e7a4713f1c687c36
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-02-25 15:37:54 +0100

    use useReducer with the cart

--- web-ecommerce/src/Data.jsx
+++ web-ecommerce/src/Data.jsx
@@ -3,73 +3,85 @@ const DATA = [
     title: "Sweet potatoes 350g",
     price : "29.90",
     desc :"Root fruit with reddish flesh reminiscent of potatoes in texture. The taste is sweet and full-bodied. Good to bake in the oven or mix in fruity pots.",
-    img : "/asset/potato.jpg"
+    img : "/asset/potato.jpg",
+    amount:1
 },
 {id:1,
     title: "Oat drink iKaffe 1L Oatly",
     price : "19.90",
     desc :"A oat drink perfect for frothing in coffee. 3% fat content.",
-    img : "/asset/oat.jpg"
+    img : "/asset/oat.jpg",
+    amount:1
 },
 {id:2,
     title: "Bacon 3-pack 420g Scan",
     price : "41.50",
     desc :"Bacon, which is smoked and salted pork loin, is a real Swedish classic that is always just as popular. This particular bacon belongs to our Scan Piggham range, which consists of extra tasty, juicy and tasty meat. And of course all our products contain only meat from Swedish farms.",
-    img : "/asset/bacon.jpg"
+    img : "/asset/bacon.jpg",
+    amount:1
 },
 {id:3,
     title: "Cashews 500g",
     price : "96.50",
     desc :"Energy (kcal) 600 kcal, Energy (kJ) 2450 kJ, Fat 46 g, Of which saturated fat 9 g, Carbohydrates 26 g, Of which sugar 9 g, Fiber 3 g, Protein 19 g, Salt 40 g, Phosphorus 490 mg, Magnesium 260 mg, Iron 6 mg, Zinc 5.60 mg",
-    img : "/asset/nat.jpg"
+    img : "/asset/nat.jpg",
+    amount:1
 },
 {id:4,
     title: "Wettex Classic 10-pack",
     price : "29.90",
     desc :"Wettex is 100% biodegradable and compostable",
-    img : "/asset/Wet.jpg"
+    img : "/asset/Wet.jpg",
+    amount:1
 },
 {id:5,
     title: "Banana Eko 180g",
     price : "26.50",
     desc :"Country of origin Ecuador",
-    img : "/asset/banana.jpg"
+    img : "/asset/banana.jpg",
+    amount:1
 },
 {id:6,
     title: "Tomato 110g",
     price : "37.50",
     desc :"An all-round working tomato that can vary a little in taste depending on the variety. Suitable for salads, seafood dishes or as a sandwich spread with cheese.",
-    img : "/asset/tomato.jpg"
+    img : "/asset/tomato.jpg",
+    amount:1
 },
 {id:7,
     title: "Yogurt Blueberry 2.7% 250g",
     price : "10.50",
     desc :"Ingredients: Swedish highly pasteurized MILK, sugar 8.1%, blueberries 6.0%, modified corn starch, thickener (pectin), aroma, acidification culture, vitamin D. The blueberries are Swedish.",
-    img : "/asset/yo.jpg"
+    img : "/asset/yo.jpg",
+    amount:1
 },
 {id:8,
     title: "Soft drink Pepsi Max 1.5L",
     price : "18.50",
     desc :"Ingredients: Carbonated water, dye (E 150d), sweeteners (aspartame, acesulfame k), acidity regulators (phosphoric acid, citric acid, sodium citrate), flavoring agents (incl. Caffeine). Contains a source of phenylalanine.",
-    img : "/asset/max.jpg"
+    img : "/asset/max.jpg",
+    amount:1
 },
 {id:9,
     title: "Brewed coffee Franskrost 500g",
     price : "52",
     desc :"Extra dark rust - Nuanced & Full. Scent of dark chocolate. Powerful and nuanced taste with a hint of vanilla. Long aftertaste with balanced acidity. Franskrost is a 100% sustainable certified (UTZ Certified) and 100% climate-compensated coffee.",
-    img : "/asset/coffee.jpg"
+    img : "/asset/coffee.jpg",
+    amount:1
 },
 {id:10,
     title: "Green lettuce 130g",
     price : "20.90",
     desc :"Country of origin Netherlands",
-    img : "/asset/green.jpg"
+    img : "/asset/green.jpg",
+    amount:1
 },
 {id:11,
     title: "Eggs M 6-p Crown",
     price : "16",
     desc :"Nutritional value per 100 g Energy (kcal) 140 kcal, Energy (kJ) 580 kJ, Fat 9.7 g, Of which saturated fat 2.6 g, Carbohydrate 0.5 g, Of which sugar 0 g, Protein 12 g, Salt 0.36 g",
-    img : "/asset/agg.jpg"
+    img : "/asset/agg.jpg",
+    amount:1
 }
 ]
 
