0073fba0b78a08cbe11343edc02ed6cf71f9a1b5
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-02-26 23:43:59 +0100

    Put key in the products

---
+++ web-ecommerce/src/component/card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from 'react-router-dom';
+
+const Cards = ({ item, handleClick }) => {
+  const { title, price, img } = item;
+  return (
+    <div className="cards">
+      <div className="image_box">
+        <img src={img} alt="" />
+      </div>
+      <div className="details">
+      <NavLink to= {`/products/${item.id}`} class="btn btn-outline-primary"><p>{title}</p></NavLink>
+        <p>Price - {price} Kr</p>
+        <button onClick={() => handleClick(item)}>Add to Cart</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
