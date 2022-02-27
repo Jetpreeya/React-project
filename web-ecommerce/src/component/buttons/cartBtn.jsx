73ea4049eca34f2db415da69a91ee0950d67e432
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-02-27 19:19:26 +0100

    Show numbet at cartBtn

--- web-ecommerce/src/component/buttons/cartBtn.jsx
+++ web-ecommerce/src/component/buttons/cartBtn.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { useGlobalContext } from '../context'
 
 const CartBtn = () => {
+    const { amount } = useGlobalContext()
     return (
         <>       
             <div class="button" >
               <a class="nav-link active " aria-current="page" href="/Cart"><i class='fa fa-shopping-cart me-1 fa-2x'></i>
-              Cart(0)</a>
+              {amount}</a>
             </div>
-            
-            
+                        
         </>
     )
 }
