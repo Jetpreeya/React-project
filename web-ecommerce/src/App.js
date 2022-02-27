a8f3e29c6faf0626f86bbfd8b0ead351ba2ced92
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-02-27 19:33:16 +0100

    I can add the product into the cart

--- web-ecommerce/src/App.js
+++ web-ecommerce/src/App.js
@@ -1,26 +1,58 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
 import Navbar from './component/Narbar';
 import Home from './component/Home';
 import Product from './component/Products';
 import ProductsDetail from './component/ProductsDetail';
 import Footer from './component/Footer';
 import { Routes, Route} from "react-router-dom";
+import Cart from './component/cart';
 import CartContainer from './component/CartContainer';
 
+ 
+/*Routing with React */
 function App() {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
+ 
+  const handleClick = (item) => {
+    if (cart.indexOf(item) !== -1) return;
+    setCart([...cart, item]);
+  };
+ 
+  const handleChange = (item, d) => {
+    const ind = cart.indexOf(item);
+    const arr = cart;
+    arr[ind].amount += d;
+ 
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCart([...arr]);
+  };
+ 
+  useEffect(() => {
+    console.log("cart change");
+  }, [cart]);
+ 
   return (
-    <>
-    <Navbar/>
-    <Routes>
+    <React.Fragment>
+      {/* <Navbar/> */}
+      <Navbar setShow={setShow} size={cart.length} />
+      <Routes>
         <Route exact path="/" element={<Home/>} />
         <Route exact path="/Products" component={ <Product/>} />
         <Route exact path="/Products/:id" element={ <ProductsDetail/>} />
         <Route exact path="/Cart" element={ <CartContainer/>} />
       </Routes>
-      <Product/>
+      {show ? (
+        <Product handleClick={handleClick} />
+      ) : (
+        <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+      )}
     <Footer/>
-    </>
+    </React.Fragment>
+ 
   );
 }
-
+ 
 export default App;
+
