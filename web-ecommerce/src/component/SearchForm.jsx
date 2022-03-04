35f9e2c96492e0f4a36cb68a78c3dbd4737d3d3d
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-03-04 23:41:22 +0100

    Search Bar

--- web-ecommerce/src/component/SearchForm.jsx
+++ web-ecommerce/src/component/SearchForm.jsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/SearchForm.css"
+import DATA from "../Data";
 
 const SearchBar = () => {
-   
+    const [filter, setFilter] = useState('');
+
+    const searchText = (event) => {
+        setFilter(event.target.value);
+    }
+    let dataSearch = DATA.filter(item => {
+        return Object.keys(item).some(key =>
+            item[key].toString().toLowerCase().includes(filter.toString().toLowerCase())
+        )
+    });
     return (
-        <>
-            <div className="input-group input-group-rounded">
-                <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" size="20%"/>
+            <div className="input-group input-group-rounded py-4">
+                  <input type="text" className="from-control" value={filter} onChange={searchText.bind(this)}/>
                 <span className="input-group-text border-4" id="search-addon">
                     <i className="fa fa-search"></i>
                 </span>
-            </div>
+             </div>
+            )
+    }
 
-        </>
-    )
-}
 
 export default SearchBar  
