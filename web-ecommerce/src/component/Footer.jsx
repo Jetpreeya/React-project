f43c0fcbc708175c287fe7475fdc78bc4f7a0fcd
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-03-15 13:50:16 +0100

    Footer part done

--- web-ecommerce/src/component/Footer.jsx
+++ web-ecommerce/src/component/Footer.jsx
@@ -1,42 +1,41 @@
 import React from 'react';
 
-const Footer = () =>{
-    return(
+const Footer = () => {
+  return (
+    <footer className="page-footer font-small special-color-dark pt-4 bg-info">
+      <div className="container">
+        <ul className="list-unstyled list-inline text-center">
+          <li className="list-inline-item">
+            <a className="btn-floating btn-fb mx-1" href="facebook.com">
+              <i className="fa fa-facebook"> Facebook </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-tw mx-1" href="twitter.com">
+              <i className="fa fa-twitter"> Twitter </i>
+            </a>
+          </li>
+          <li className="list-inline-item">
+            <a className="btn-floating btn-li mx-1 " href="linkedin.com">
+              <i className="fa fa-linkedin"> LinkedIn </i>
+            </a>
+          </li>
+        </ul>
 
-<footer className="page-footer font-small special-color-dark pt-4">
+      </div>
+     
+      <div className="footer-copyright text-center py-3">© {new Date().getFullYear()} Copyright: 
+      <ul className="list-unstyled list-inline text-center text-light">
+      <li ><a className="navbar-brand text-light" href="/"> Home</a></li>
+      <li ><a className="navbar-brand text-light" href="/Products">Products</a></li>
+        </ul>
 
+      </div>
 
-  <div className="container">
+    </footer>
 
-
-    <ul className="list-unstyled list-inline text-center">
-      <li className="list-inline-item">
-        <a className="btn-floating btn-fb mx-1" href="facebook.com">
-          <i className="fa fa-facebook"> Facebook </i>
-        </a>
-      </li>
-      <li className="list-inline-item">
-        <a className="btn-floating btn-tw mx-1" href="twitter.com">
-          <i className="fa fa-twitter">Twitter </i>
-        </a>
-      </li>
-      <li className="list-inline-item">
-        <a className="btn-floating btn-li mx-1" href="linkedin.com">
-          <i className="fa fa-linkedin">LinkedIn </i>
-        </a>
-      </li>
-    </ul>
-
-  </div>
-  <div className="footer-copyright text-center py-3">© {new Date().getFullYear()} Copyright:
-    <a className="navbar-brand" href="/">Home</a>
-    <a className="navbar-brand" href="/Products">Products</a>
-  </div>
-
-</footer>
-       
-    )
+  )
 }
 
-export default Footer 
+export default Footer
 
