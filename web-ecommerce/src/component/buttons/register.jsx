99cb9f6fae23829b5d6e89bbf8d2200966f082d6
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-03-16 15:12:03 +0100

    Register with firebase

--- web-ecommerce/src/component/buttons/register.jsx
+++ web-ecommerce/src/component/buttons/register.jsx
@@ -1,58 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { Navigate } from 'react-router-dom'
+import firebaseConfig from '../../config'
 
 const Register = () => {
-    return (
-        <>
-            <button type="button" className="btn btn-outline-info ms-2" data-bs-toggle="modal" data-bs-target="#NewModal">
-            <i className="fa fa-sign-in me-1 fa-2x"></i> Register
-            </button>
+    const [currentUser, setCurrentUser] = useState(null);
 
-            <div className="modal fade" id="NewModal" tabIndex="-1" aria-labelledby="NewModal" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="NewModal">Registration</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div className="modal-body">
-                            <form>
-                            <div className="mb-3">
-                                    <label htmlFor="NewInputName" className="form-label">First Name</label>
-                                    <input type="text" className="form-control" id="NewInputName"/>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="NewInputLast" className="form-label">Last Name</label>
-                                    <input type="text" className="form-control" id="NewInputLast"/>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="NewInputEmail1" className="form-label">Email address</label>
-                                    <input type="email" className="form-control" id="NewInputEmail1" aria-describedby="emailHelp"/>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="NewInputPassword1" className="form-label">Password</label>
-                                    <input type="password" className="form-control" id="NewInputPassword1"/>
-                                </div>
-        
-                                <button type="submit" className="btn btn-success w-30 ms-3 ">Register</button>
-                                <button type="submit" className="btn btn-danger w-30 ms-3">Cancle</button>
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { email, password } = e.target.elements;
 
-                                <div className= "modal-body">
-                            <button className="btn btn-primary w-100 mb-4">
-                                <span className="fa fa-google me-2"></span> Register With Google 
-                            </button>
-                            <button className="btn btn-primary w-100 mb-2">
-                                <span className="fa fa-facebook me-2"></span> Register With Facebook 
-                            </button>
+        try {
+
+            firebaseConfig.auth().createUserWithEmailAndPassword(email.value, password.value);
+            setCurrentUser(true);
+
+        } catch (error) {
+            alert(error);
+        }
+    }
+// Should I use Navigate or Link ?
+    if (currentUser) {
+        return <Navigate to = "/dashboard" />
+    }
+        return (
+            <>
+                <button type="button" className="btn btn-outline-info ms-2" data-bs-toggle="modal" data-bs-target="#NewModal">
+                    <i className="fa fa-sign-in me-1 fa-2x"></i> Register
+                </button>
+
+                <div className="modal fade" id="NewModal" tabIndex="-1" aria-labelledby="NewModal" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="NewModal">Registration</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
-                            </form>
+                            <div className="modal-body">
+                                <form onSubmit={handleSubmit}>
+                                    <div className="mb-3">
+                                        <label htmlFor="NewInputName" className="form-label">First Name</label>
+                                        <input type="text" className="form-control" id="NewInputName" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="NewInputLast" className="form-label">Last Name</label>
+                                        <input type="text" className="form-control" id="NewInputLast" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="NewInputEmail1" className="form-label">Email address</label>
+                                        <input type="email" name="email" className="form-control" id="NewInputEmail1" aria-describedby="emailHelp" />
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="NewInputPassword1" className="form-label">Password</label>
+                                        <input type="password" name="password" className="form-control" id="NewInputPassword1" />
+                                    </div>
+
+                                    <button type="submit" className="btn btn-success w-30 ms-3 ">Register</button>
+                                    <button type="submit" className="btn btn-danger w-30 ms-3">Cancle</button>
+
+                                    <div className="modal-body">
+                                        <button className="btn btn-primary w-100 mb-4">
+                                            <span className="fa fa-google me-2"></span> Register With Google
+                                        </button>
+                                        <button className="btn btn-primary w-100 mb-2">
+                                            <span className="fa fa-facebook me-2"></span> Register With Facebook
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </>
+            </>
 
-    )
-}
+        )
+    }
 
-export default Register
-
+    export default Register
---
+++ web-ecommerce/src/dashboard.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import { Navigate } from 'react-router-dom'
+import { AuthContext } from './component/buttons/Auth'
+import firebaseConfig from './config'
+
+const DashBoard = () => {
+    const { currentUser } = useContext(AuthContext);
+
+    if (!currentUser) {
+        return <Navigate to ="/" />;
+    }
+
+    return (
+        <div>
+            <div className="container mt-5">
+            <div className = "text-center">
+                <h1>Welcome to Jet E-commerce</h1>
+                <p> If you can see this you're logged in.</p>
+                <button onClick={() => firebaseConfig.auth().signOut()} className="btn btn-dark">Sign Out</button>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default DashBoard;
