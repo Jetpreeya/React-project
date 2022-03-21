331afca59ad5dd11e44fa9912406659014a1b95d
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-03-21 15:58:35 +0100

    Use Link for deployment

--- web-ecommerce/src/component/buttons/cartIcon.jsx
+++ web-ecommerce/src/component/buttons/cartIcon.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useGlobalContext } from '../context/context'
+import { Link } from "react-router-dom";
 
 const CartBtn = () => {
     const { amount } = useGlobalContext()
     return (
         <>       
             <div className="button" >
-              <a className="nav-link active " aria-current="page" href="/CartContainer"><i className='fa fa-inbox me-1 fa-2x'></i>
-              {amount}</a>
+            <div className ="btn btn-outline-warning ms-1">
+            <Link to="/CartContainer"><i className='fa fa-inbox me-1 fa-2x'></i>
+              {amount}</Link>
+            </div>
             </div>
 
 
