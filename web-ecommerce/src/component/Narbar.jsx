b9663dc0f99fa4848dfd4e4b66a50995d8c5b286
Jetpreeya <Jetpreeya.Norkaew@gmail.com>
2022-03-23 13:23:41 +0100

    Delete cartcontainer

--- web-ecommerce/src/component/Narbar.jsx
+++ web-ecommerce/src/component/Narbar.jsx
@@ -1,12 +1,11 @@
-import React from "react";
+import React  from "react";
 import Login from "./buttons/login";
 import Register from "./buttons/register";
 import "./styles/navbar.css";
 import { Link } from "react-router-dom";
-import CartBtn from "./buttons/cartIcon";
+// import CartBtn from "./buttons/cartIcon";
 
 /*I use Link to connect cart with route cart */
-
 const Navbar = ({ setShow, size }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light py-3 shadow-sm">
@@ -23,10 +22,9 @@ const Navbar = ({ setShow, size }) => {
           <Link to="/Fetch"><i className='fa fa-tags fa-2x'></i>Fetch Product</Link>
           </div>
           </div>
-        
         <Register />
         <Login />
-        <CartBtn />
+        {/* <CartBtn /> */}
         <div className="cart nav-link" onClick={() => (setShow(false))}>
           <Link to="/Cart"><i className="fa fa-shopping-cart me-2 fa-2x"></i></Link>
           <span>{size}</span>
